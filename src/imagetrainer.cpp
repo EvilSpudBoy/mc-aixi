ef7a08f1c19d992912a8d3fef48d5d90bfc84b89
Michael Stevens <michael.stevens@gmail.com>
2023-03-15 18:19:19 -0400

    add imagetrainer environment

---
+++ src/imagetrainer.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Michael Stevens on 3/15/23.
+//
+
+#include "imagetrainer.hpp"
---
+++ src/imagetrainer.hpp
@@ -0,0 +1,14 @@
+//
+// Created by Michael Stevens on 3/15/23.
+//
+
+#ifndef MC_AIXI_IMAGETRAINER_HPP
+#define MC_AIXI_IMAGETRAINER_HPP
+
+
+class imagetrainer {
+
+};
+
+
+#endif //MC_AIXI_IMAGETRAINER_HPP
