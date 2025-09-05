fae655b88ca9ea40712e5d0a67e9640450b322ff
EvilSpudBoy <EvilSpudBoy@users.noreply.github.com>
2025-09-05 09:12:29 -0400

    add an xcode project.

---
+++ xcode/mc-aixi/mc-aixi/mc_aixiApp.swift
@@ -0,0 +1,32 @@
+//
+//  mc_aixiApp.swift
+//  mc-aixi
+//
+//  Created by Michael Stevens on 9/5/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct mc_aixiApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ xcode/mc-aixi/mc-aixiTests/mc_aixiTests.swift
@@ -0,0 +1,16 @@
+//
+//  mc_aixiTests.swift
+//  mc-aixiTests
+//
+//  Created by Michael Stevens on 9/5/25.
+//
+
+import Testing
+
+struct mc_aixiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
